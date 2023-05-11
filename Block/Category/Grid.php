0ae1651e0a2512514c61f7f15627e1000e78f93e
2910george <121615040+2910george@users.noreply.github.com>
2023-05-11 14:40:48 +0530

    pagination

--- Block/Category/Grid.php
+++ Block/Category/Grid.php
@@ -5,16 +5,30 @@ class Block_Vender_Grid extends Block_Core_Layout
 	public function __construct()
 	{
 		parent::__construct();
-		$this->setTemplate('vendor/grid.phtml');
+		$this->setTemplate('category/grid.phtml');
 	}
 
 	public function prepareData()
 	{
-		$sql = "SELECT * FROM `vender`";
+		
+		$pager = $this->getPager();
+		$sql1 = "SELECT COUNT(`category_id`) FROM `category`";
+		$total = Ccc::getModel('Core_Adapter')->fetchOne($sql1);
+		$count = $total['COUNT(`category_id`)'];
+
+		$pager->setTotalRecords($count)->calculate();
+
+		$sql2 = "SELECT * FROM `category` LIMIT $pager->startLimit,$pager->recordPerPage ";
+		$category_row = Ccc::getModel('category_row');
+		$categorys = $category_row->fetchAll($sql2);
+		$this->setData(['categorys'=>$categorys]);
+		return $this;
+
+		/*$sql = "SELECT * FROM `vender`";
 		$vender = Ccc::getModel('vender_row');
 		$venders = $vender->fetchAll($vender);
 		$vender->setData(['vendors'=>$vendors]);
-		return $this;
+		return $this;*/
 	}
 }
 
--- Block/customer/Grid.php
+++ Block/customer/Grid.php
@@ -12,11 +12,27 @@ class Block_Customer_Grid  extends Block_Core_Layout
 
 	public function prepareData()
 	{
-		$sql = "SELECT * FROM `customer` JOIN `customer_address` ON customer.customer_id=customer_address.customer_id";
+		
+		$pager = $this->getPager();
+		$sql1 = "SELECT COUNT(`customer_id`) FROM `customer`";
+		$total = Ccc::getModel('Core_Adapter')->fetchOne($sql1);
+		$count = $total['COUNT(`customer_id`)'];
+
+		$pager->setTotalRecords($count)->calculate();
+
+		$sql2 = "SELECT * FROM `customer` JOIN `customer_address` ON customer.customer_id = customer_address.customer_id LIMIT $pager->startLimit,$pager->recordPerPage ";
+		$customer_row = Ccc::getModel('customer_row');
+		$customers = $vendor_row->fetchAll($sql2);
+		$this->setData(['customers'=>$customers]);
+		return $this;
+
+
+
+		/*$sql = "SELECT * FROM `customer` JOIN `customer_address` ON customer.customer_id=customer_address.customer_id";
 		$customer_row = Ccc::getModel('Customer_Row');
 		$customers = $customer_row->fetchAll($sql);
 		$this->setData(['customers'=>$customers]);
-		return $this; 
+		return $this;*/ 
 	}
 
 
